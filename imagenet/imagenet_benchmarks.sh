c558bc2c4ed357d4c1f5721de6eb8fc5753da784
Baolin Li <baolin.li1994@gmail.com>
2023-03-02 16:56:13 -0500

    update benchmarks.sh

--- imagenet/imagenet_benchmarks.sh
+++ imagenet/imagenet_benchmarks.sh
@@ -3,27 +3,37 @@
 python main.py -a resnet50 \
 -b 256 --benchmarking --gpu-type $1 \
 --multiprocessing-distributed --world-size 1 --rank 0 \
---num-gpu 4 imagenet
+--num-gpu 4 imagenet &&
+
+sleep 30
 
 python main.py -a vit_b_16 \
 -b 256 --benchmarking --gpu-type $1 \
 --multiprocessing-distributed --world-size 1 --rank 0 \
---num-gpu 4 imagenet
+--num-gpu 4 imagenet &&
+
+sleep 30
 
 python main.py -a vgg19_bn \
 -b 256 --benchmarking --gpu-type $1 \
 --multiprocessing-distributed --world-size 1 --rank 0 \
---num-gpu 4 imagenet
+--num-gpu 4 imagenet &&
+
+sleep 30
 
 python main.py -a resnext50_32x4d \
 -b 256 --benchmarking --gpu-type $1 \
 --multiprocessing-distributed --world-size 1 --rank 0 \
---num-gpu 4 imagenet
+--num-gpu 4 imagenet &&
+
+sleep 30
 
 python main.py -a inception_v3 \
 -b 256 --benchmarking --gpu-type $1 \
 --multiprocessing-distributed --world-size 1 --rank 0 \
---num-gpu 4 imagenet
+--num-gpu 4 imagenet &&
+
+sleep 30
 
 python main.py -a shufflenet_v2_x1_5 \
 -b 256 --benchmarking --gpu-type $1 \
